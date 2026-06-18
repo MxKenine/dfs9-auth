aec9528537e09658bac474094615d3325852c784
MxKenine <267941369+MxKenine@users.noreply.github.com>
2026-06-18 16:18:05 +0200

    stable1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,21 @@
-import Connexion from "./pages/Connexion"
-import Navbar from "./components/Navbar"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Connexion from './pages/Connexion.jsx'
+import Home from './pages/Home.jsx'
+import Navbar from './components/Navbar.jsx'
+import Cv from './pages/Cv.jsx'
 
-function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="" element={<App />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+export default function App() {
+    return (
+        <>
+            <BrowserRouter>
+                <Navbar />
+                <Routes>
+                    <Route path='' element={<Home />} />
+                    <Route path='/connexion' element={<Connexion />} />
+                    <Route path='/cv' element={<Cv />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
 }
-export default App
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,8 +4,9 @@ import { Link } from 'react-router-dom'
 export default function Navbar() {
     return (
         <nav>
-            <Link to="/">App</Link>
+            <Link to="/">Home</Link>
             <Link to="/connexion">Connexion</Link>
+            <Link to="/cv">CV</Link>
         </nav>
     )
 }
